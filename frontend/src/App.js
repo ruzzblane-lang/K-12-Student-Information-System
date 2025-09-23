f2c15682cb323510e65cc32678c4aa1762881d95
ruzzblane-lang <ruzzblane-lang@github.com>
2025-09-23 20:22:14 +0700

    Wrap App component with AuthProvider to enable authentication context across the application.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './hooks/useAuth';
 import MainLayout from './layouts/MainLayout';
 import StudentsPage from './pages/StudentsPage';
 import YearbookPortalPage from './pages/YearbookPortalPage';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <MainLayout>
-          <Routes>
-            <Route path="/" element={<StudentsPage />} />
-            <Route path="/students" element={<StudentsPage />} />
-            <Route path="/yearbooks" element={<YearbookPortalPage />} />
-            <Route path="/yearbooks/:schoolId" element={<YearbookPortalPage />} />
-          </Routes>
-        </MainLayout>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <MainLayout>
+            <Routes>
+              <Route path="/" element={<StudentsPage />} />
+              <Route path="/students" element={<StudentsPage />} />
+              <Route path="/yearbooks" element={<YearbookPortalPage />} />
+              <Route path="/yearbooks/:schoolId" element={<YearbookPortalPage />} />
+            </Routes>
+          </MainLayout>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
